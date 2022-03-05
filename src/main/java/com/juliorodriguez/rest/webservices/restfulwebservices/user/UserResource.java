91edf465f55b6d73f6c84a0a540e693a2e5a55d0
Julio <juliorodriguezramirez@gmail.com>
2022-03-06 00:29:43 +0100

    POST method to create User Resource

--- src/main/java/com/juliorodriguez/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/juliorodriguez/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +23,12 @@ public class UserResource {
 	@GetMapping(path="/users/{id}")
 	public User retriveAllUsers(@PathVariable int id) {
 		return srv.findOne(id); 
+	}
+	
+	@PostMapping(path="/users")
+	public void createUser(@RequestBody User user) {
+		// No pasar id en el path, la identificación se ha de asignar en el backEnd
+		User savedUser = srv.save(user); 
 	}
 	
 }

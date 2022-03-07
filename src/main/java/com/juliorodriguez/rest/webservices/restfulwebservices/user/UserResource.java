bef7b35d13dfbbdeeee20a9286674f39a4c9cf41
Julio <juliorodriguezramirez@gmail.com>
2022-03-07 12:51:59 +0100

    Enhancing POST Method to return correct HTTP Status Code and Location

--- src/main/java/com/juliorodriguez/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/juliorodriguez/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.juliorodriguez.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -22,13 +25,22 @@ public class UserResource {
 	
 	@GetMapping(path="/users/{id}")
 	public User retriveAllUsers(@PathVariable int id) {
-		return srv.findOne(id); 
+		return srv.findOne(id);
 	}
 	
 	@PostMapping(path="/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 		// No pasar id en el path, la identificación se ha de asignar en el backEnd
-		User savedUser = srv.save(user); 
+		User savedUser = srv.save(user);
+		
+		// CREATED
+		// resource location created  savedUser.getId()
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 }
